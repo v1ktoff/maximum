2f8f45655a48bcba23cbb12034d077d2a34eedf7
v1ktoff <110956360+v1ktoff@users.noreply.github.com>
2023-10-09 12:38:05 -0600

    Maximum number

---
+++ Maximum.py
@@ -0,0 +1,10 @@
+#!/usr/env python3
+#Maximun algorithm
+#By: v1ktoff
+#License GNU/Public
+data =[10,69,420,666,42069666]
+Maximun=data[0]
+for x in data:
+    if x>Maximun:
+        Maximun=x
+print(Maximun)
